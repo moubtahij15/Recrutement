3b47dabad9658e0c8620f385aefb4c54b4150629
Moubtahij Otmane <77882695+moubtahij15@users.noreply.github.com>
2023-01-18 15:09:23 +0100

    Delete src directory

--- src/main/java/com/example/recrutementbackend/Helpers/Enum.java
+++
@@ -1,6 +0,0 @@
-package com.example.recrutementbackend.Helpers;
-
-public interface Enum {
-    enum Etat {EN_ATTENTE, VALIDER}
-    enum Role {AGENT, RECRUTEUR}
-}
--- src/main/java/com/example/recrutementbackend/Repositories/AgentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.recrutementbackend.Repositories;
-
-import com.example.recrutementbackend.Entities.Agent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgentRepository extends JpaRepository<Agent, Long> {
-    public Agent findByEmail(String email);
-}
